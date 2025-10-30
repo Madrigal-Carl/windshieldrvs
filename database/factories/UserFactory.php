9c92b9e1dce52e553dfa38c7b9f26afde89cd273
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-10-30 13:22:07 +0800

    change 1.8

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'email' => $this->faker->safeEmail(),
+            'email' => $this->faker->email(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
